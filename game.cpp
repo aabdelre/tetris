50128447421a5fd03936b94797aa8acce3c26c10
Ahmed Abdelrehim <88915626+aabdelre@users.noreply.github.com>
2023-03-12 17:38:28 +0000

    Create game.cpp

---
+++ game.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+
+#define WIDTH 10
+#define HEIGHT 22
+#define SEEN_HEIGHT 20
+
+struct GameState {
+    int board[HEIGHT][WIDTH];
+};
+
+void update_game_play() {
+    
+}
+
+void update_game() {
+
+}
+
+int main() {
+
+}
