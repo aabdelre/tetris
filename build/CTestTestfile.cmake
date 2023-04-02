0433a5cd62b545c32c64809b37f0e499558a6236
aabdelre <aabdelre@hamilton.edu>
2023-04-02 01:13:47 +0100

    Create CMakeLists

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Ahmed Abdelrehim/OneDrive/Documents/GitHub/tetris
+# Build directory: C:/Users/Ahmed Abdelrehim/OneDrive/Documents/GitHub/tetris/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
